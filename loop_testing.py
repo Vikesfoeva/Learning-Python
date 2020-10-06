415d8d7bd0522c53c39310af35a91820d592fb76
Brandon Lenz <lenzb@oregonstate.edu>
2020-10-05 22:54:13 -0400

    Added a try except

--- loop_testing.py
+++ loop_testing.py
@@ -1,4 +1,9 @@
-user_in = int(input())
+print("Please enter an integer")
+try:
+    user_in = int(input())
+except:
+    print("Please ensure you are entering a valid integer")
+    exit()
 
 for i in range(1, user_in, 1):
     if user_in % i == 0:
